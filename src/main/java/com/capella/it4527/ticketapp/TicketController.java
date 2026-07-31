3396b97389f117e8b0fb578cf008f76279cb28f9
Zainab Omar <zainab.alshaikhli01@gmail.com>
2026-07-31 14:41:44 -0500

    update put and get methods

--- src/main/java/com/capella/it4527/ticketapp/TicketController.java
+++ src/main/java/com/capella/it4527/ticketapp/TicketController.java
@@ -16,48 +16,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 public class TicketController {
 
-    @RequestMapping("/ticket/put")
-    public String putTicket(
-            @RequestParam int id,
-            @RequestParam String title,
-            @RequestParam String type,
-            @RequestParam int priority,
-            @RequestParam(required = false, defaultValue = "Open") String status,
-            @RequestParam(required = false) String description,
-            @RequestParam(required = false, defaultValue = "0") int error,
-            @RequestParam(required = false) String task) {
+    @PostMapping("/ticket/put")
+    public String putTicket(@RequestBody Ticket ticket) {
 
-        Ticket ticket;
-
-        if (type.equalsIgnoreCase("bug")) {
-            BugTicket bugTicket = new BugTicket();
-
-            bugTicket.setId(id);
-            bugTicket.setTitle(title);
-            bugTicket.setPriority(priority);
-            bugTicket.setStatus(status);
-            bugTicket.setDescription(description);
-            bugTicket.setError(error);
-
-            ticket = bugTicket;
-
-        } else if (type.equalsIgnoreCase("task")) {
-            TaskTicket taskTicket = new TaskTicket();
-
-            taskTicket.setId(id);
-            taskTicket.setTitle(title);
-            taskTicket.setPriority(priority);
-            taskTicket.setStatus(status);
-            taskTicket.setTask(task);
-
-            ticket = taskTicket;
-
-        } else {
-            return "Invalid ticket type. Use bug or task.";
-        }
-
-        String fileName = id + ".txt";
-        Path filePath = Path.of(fileName);
+       String fileName = ticket.getId() + ".txt";
+       Path filePath = Path.of(fileName);
 
         try {
             Files.writeString(filePath, ticket.toString());
@@ -67,7 +30,7 @@ public class TicketController {
         }
     }
 
-    @RequestMapping("/ticket/get")
+    @GetMapping("/ticket/get")
     public Ticket getTicket(@RequestParam int id) {
 
         Path filePath = Path.of(id + ".txt");
