0a469fab3228c5e0d9a57888b0f4fbfa11ec1ac5
Zainab Omar <zainab.alshaikhli01@gmail.com>
2026-07-31 14:33:07 -0500

    add requried annotations to update TicketContr oller class

--- src/main/java/com/capella/it4527/ticketapp/TicketController.java
+++ src/main/java/com/capella/it4527/ticketapp/TicketController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 @RestController
 public class TicketController {
 
